2af3873f43577b83e6994a1111a1c1c2272a2e08
Matthew Hughes <me@matthewhughes.co.uk>
2013-02-17 15:21:37 +0000

    Committing because my computer is terrible and it may crash at any given moment.

--- src/WelcomeAndFileActions.java
+++ src/WelcomeAndFileActions.java
@@ -6,7 +6,29 @@
  * To change this template use File | Settings | File Templates.
  */
 public class WelcomeAndFileActions {
-    public void Welcome(){
-    System.out.println("");
+    static void Welcome(){
+    System.out.println("Welcome to the Java!Java!JSON compiler");
+    System.out.println("By Matthew Hughes");
+    System.out.println("http://matthewhughes.co.uk");
+    System.out.println("me@matthewhughes.co.uk");
     }
+    static void CheckArgs(String[] args) {
+        if(args.length < 1){
+            System.out.println("Not enough arguments");
+            System.out.println("Correct usage is JJJCompiler <code.json>");
+            System.out.println("Leaving Now. Bye!");
+            System.exit(1);
+        }
+    }
+    static String GetPath(String Filename){
+        // Yes, this is a horrible, horrible hack.
+        // Yes, this basically means that this code won't really work on anyone else's computer.
+        // Yes, I'm a fuckwit.
+        // Yes, I'll fix it later.
+
+        String FileAndPath = "/home/matthew/IdeaProjects/JJJCompiler/src/" + Filename;
+        return Filename;
+    }
+    static
+
 }
