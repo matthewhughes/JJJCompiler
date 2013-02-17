d677541572e34141da73af5b224cf7afad84abc0
Matthew Hughes <me@matthewhughes.co.uk>
2013-02-17 16:49:06 +0000

    Added JSON Simple library.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	WelcomeAndFileActions WAFA = new WelcomeAndFileActions();
+    JSONManipulations JM = new JSONManipulations();
     WAFA.Welcome();
     WAFA.CheckArgs(args);
     String FilePath = WAFA.GetPath(args[0]);
