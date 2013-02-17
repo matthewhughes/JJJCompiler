3a3512ae3c7d2926abe4e2a9478dbec3334991e0
Matthew Hughes <matthew@matthew-Lenovo-G575.(none)>
2013-02-17 15:11:04 +0000

    First commit

---
+++ src/WelcomeAndFileActions.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: matthew
+ * Date: 17/02/13
+ * Time: 15:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class WelcomeAndFileActions {
+    public void Welcome(){
+    System.out.println("");
+    }
+}
