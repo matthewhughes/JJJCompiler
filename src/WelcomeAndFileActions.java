c0f7b835b22dac89fa769c24f71f8091a6e0b222
Matthew Hughes <me@matthewhughes.co.uk>
2013-02-17 15:28:40 +0000

    Finished a few methods. Whacking it into Github.

--- src/WelcomeAndFileActions.java
+++ src/WelcomeAndFileActions.java
@@ -1,10 +1,6 @@
-/**
- * Created with IntelliJ IDEA.
- * User: matthew
- * Date: 17/02/13
- * Time: 15:08
- * To change this template use File | Settings | File Templates.
- */
+import java.io.*;
+import java.util.Scanner;
+
 public class WelcomeAndFileActions {
     static void Welcome(){
     System.out.println("Welcome to the Java!Java!JSON compiler");
@@ -29,6 +25,20 @@ public class WelcomeAndFileActions {
         String FileAndPath = "/home/matthew/IdeaProjects/JJJCompiler/src/" + Filename;
         return Filename;
     }
-    static
+    static String OpenFileAndConvert(String FileAndPath) {
+        String ContentsOfFile = null;
+        try {
+            // Full disclosure. This one-liner was borrowed from Stack Overflow.
+            // http://stackoverflow.com/questions/326390/how-to-create-a-java-string-from-the-contents-of-a-file
+            ContentsOfFile = new Scanner(new File(FileAndPath), "UTF-8").useDelimiter("\\A").next();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("File Not Found.");
+            System.out.println("Remember that file names are case sensitive.");
+            System.out.println("Exiting Now");
+            System.exit(1);
+        }
+        return ContentsOfFile;
+    }
 
 }
