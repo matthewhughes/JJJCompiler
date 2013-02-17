f28d5990e1428dfe7bd4232a5d581d25f0c75cdc
Matthew Hughes <me@matthewhughes.co.uk>
2013-02-17 23:45:50 +0000

    Started writing operands and stuff.

---
+++ src/ASMFunctions.java
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: matthew
+ * Date: 17/02/13
+ * Time: 22:35
+ * To change this template use File | Settings | File Templates.
+ */
+public class ASMFunctions {
+
+    public String Add(String Value){
+    return "001" + Value;
+    }
+    public String Subtract(String Value){
+    return "002" + Value;
+    }
+    public String StoreDirect(String Value){
+        return "003" + Value;
+    }
+}
