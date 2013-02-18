f3ed0433978ba56705cf16ee73b1998f114122b2
Matthew Hughes <me@matthewhughes.co.uk>
2013-02-18 11:44:19 +0000

    Started writing operands and stuff.

--- src/ASMFunctions.java
+++ src/ASMFunctions.java
@@ -7,13 +7,17 @@
  */
 public class ASMFunctions {
 
-    public String Add(String Value){
+    String Add(String Value){
     return "001" + Value;
     }
-    public String Subtract(String Value){
+    String Subtract(String Value){
     return "002" + Value;
     }
-    public String StoreDirect(String Value){
+    String StoreDirect(String Value){
         return "003" + Value;
     }
+    String StoreImmediate(String Value){
+        return "004" + Value;
+    }
+
 }
